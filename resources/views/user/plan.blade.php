0a0e0f0e23f11539f853c5d840a8df13ffd9e3ac
ZhineSmathew <zhinesmoolekonil@gmail.com>
2025-11-25 17:15:12 +0530

    Add toolTip With Disabled

--- resources/views/user/plan.blade.php
+++ resources/views/user/plan.blade.php
@@ -13,7 +13,19 @@
                                 @if($user->plan==$plan->id)
                                     <span class="btn btn-sm btn-primary my-auto"><i class="ti ti-check "></i></span>
                                 @else
-                                    <a href="{{route('plan.active',[$user->id,$plan->id])}}" class="btn btn-sm btn-warning my-auto" title="{{__('Click to Upgrade Plan')}}"><i class="ti ti-shopping-cart-plus"></i></a>
+                                    {{-- <a href="{{route('plan.active',[$user->id,$plan->id])}}" class="btn btn-sm btn-warning my-auto" title="{{__('Click to Upgrade Plan')}}"><i class="ti ti-shopping-cart-plus"></i></a> --}}
+                                    @if($plan->is_disable == 0)
+                                        <span data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-container="body"
+                                              title="{{__('This plan is currently disabled, Please Enable')}}">
+                                            <button class="btn btn-sm btn-secondary my-auto" disabled>
+                                                <i class="ti ti-shopping-cart-plus"></i>
+                                            </button>
+                                        </span>
+                                    @else
+                                        <a href="{{route('plan.active',[$user->id,$plan->id])}}" class="btn btn-sm btn-warning my-auto" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-container="body" title="{{__('Click to Upgrade Plan')}}">
+                                            <i class="ti ti-shopping-cart-plus"></i>
+                                        </a>
+                                    @endif
                                 @endif
                             </td>
                         </tr>
@@ -23,3 +35,9 @@
         </div>
     </div>
 </div>
+<style>
+    .tooltip {
+        z-index: 9999 !important;
+    }
+</style>
+<script src="{{ asset('js/newcustom.js') }}"></script>
